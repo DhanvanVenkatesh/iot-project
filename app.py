c5c91af9ec812e956b33778915a2d2d506066e17
DhanvanVenkatesh <92202051+DhanvanVenkatesh@users.noreply.github.com>
2023-04-09 23:35:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,47 @@
-from flask import Flask, request, jsonify
+#from flask import Flask, st, jsonify
 import numpy as np
 import pickle
+import streamlit as st
 
 model = pickle.load(open('./model1.pkl', 'rb'))
 
-app = Flask(__name__)
+#app = Flask(__name__)
+
+# @app.route('/')
 
 
-@app.route('/')
 def index():
     return "Hello world"
 
 
-@app.route('/predict', methods=['POST'])
+# @app.route('/predict', methods=['POST'])
 def predict():
 
-    nitrogen = request.form.get('nitrogen')
-    phosphorous = request.form.get('phosphorous')
-    potassium = request.form.get('potassium')
-    temp = request.form.get('temperature')
-    humidity = request.form.get('humidity')
-    ph = request.form.get('ph')
-    rainfall = request.form.get('rainfall')
+    st.title("Bank Authenticator")
+    html_temp = """
+        <div style="background-color:tomato;padding:10px">
+        <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+        </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
 
-    input_query = np.array(
-        [[nitrogen, phosphorous, potassium, temp, humidity, ph, rainfall]])
+    nitrogen = st.text_input('nitrogen', "Type Here")
+    phosphorous = st.text_input('phosphorous', "Type Here")
+    potassium = st.text_input('potassium', "Type Here")
+    temp = st.text_input('temperature', "Type Here")
+    humidity = st.text_input('humidity', "Type Here")
+    ph = st.text_input('ph', "Type Here")
+    rainfall = st.text_input('rainfall', "Type Here")
+    result = ""
+    ans = ""
+    if st.button("Predict"):
+        input_query = np.array(
+            [[nitrogen, phosphorous, potassium, temp, humidity, ph, rainfall]])
+        result = model.predict(input_query)
+        ans = result[0]
 
-    result = model.predict(input_query)
-
-    return jsonify({'Result': str(result)})
+    st.success('The output is {}'.format(ans))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    predict()
